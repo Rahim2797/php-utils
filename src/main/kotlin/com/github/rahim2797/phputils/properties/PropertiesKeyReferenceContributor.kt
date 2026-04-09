736267b9d757349002cb3f0f5f365b1ed95275b9
Rahim_Benmelouka <abderrahim.benmelouka@evedrug.eu>
2026-04-09 06:43:27 +0200

    Removes sample code and adjusts project to focus on PHP-specific utilities.

---
+++ src/main/kotlin/com/github/rahim2797/phputils/properties/PropertiesKeyReferenceContributor.kt
@@ -0,0 +1,10 @@
+package com.github.rahim2797.phputils.properties
+
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceRegistrar
+
+class PropertiesKeyReferenceContributor: PsiReferenceContributor() {
+    override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
+        TODO("Not yet implemented")
+    }
+}
